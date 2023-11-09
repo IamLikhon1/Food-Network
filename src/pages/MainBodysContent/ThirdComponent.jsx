de6a1d394b0e261d74327fdfc9917bff501dff50
IamLikhon1 <likhonadnan55@gmail.com>
2023-11-09 11:04:31 +0600

    modify

--- src/pages/MainBodysContent/ThirdComponent.jsx
+++ src/pages/MainBodysContent/ThirdComponent.jsx
@@ -4,7 +4,7 @@ const ThirdComponent = ({data}) => {
 
     return (
         <div>
-            <div className="lg:grid lg:grid-cols-2 gap-5 lg:pb-2">
+            <div className="lg:grid lg:grid-cols-2 gap-5 lg:pb-2 ">
                  {/* img */}
             <div className="h-full lg:py-5 px-2 rounded-md bg-white">
             <img className="rounded-md h-fit" src={pictureTwo} alt="" />
@@ -12,7 +12,7 @@ const ThirdComponent = ({data}) => {
            {/* img */}
            {/* text */}
            <div>
-            <h4 className="lg:mt-7 font-semibold text-lg">Supports eye health: </h4>
+            <h4 className="lg:mt-7 font-semibold text-lg mt-2">Supports eye health: </h4>
             <p className="text-base text-gray-600 lg:mt-4">Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.</p>
             <h4 className="mt-3 lg:mt-7 font-semibold text-lg">Supports eye health: </h4>
             <p className="text-base text-gray-600 lg:mt-4">Spinach contains high levels of vitamin A and other antioxidants that help maintain healthy vision and protect against age-related macular degeneration.</p>
