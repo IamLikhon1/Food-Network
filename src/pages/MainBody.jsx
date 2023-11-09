525b27d5a3a1d3d3cb4e5b2cb2c6de972b78ecb1
IamLikhon1 <likhonadnan55@gmail.com>
2023-11-09 13:26:11 +0600

    modify small device nabBar

--- src/pages/MainBody.jsx
+++ src/pages/MainBody.jsx
@@ -27,29 +27,33 @@ const MainBody = () => {
       {/* small */}
       <div className="lg:hidden drawer">
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content w-full border text-base font-medium mt-5 px-3 py-1 rounded-md bg-white shadow-sm">
+        <div className="drawer-content w-full border text-base font-medium mt-5 px-3 py-2 rounded-md bg-white shadow-sm">
           {/* Page content here */}
-          <div  className="flex justify-between items-center">
-            <div className="flex gap-2 items-center">
+          <label htmlFor="my-drawer">
+          <div className="flex justify-between items-center">
+            <div className="flex gap-2 items-center ">
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
                 <rect x="3.00439" y="2.91357" width="6" height="6" rx="2" fill="#F36C01" />
                 <rect x="3.00439" y="11.0864" width="6" height="6" rx="2" fill="#F36C01" />
                 <path d="M10.9956 5.91357C10.9956 4.25672 12.3388 2.91357 13.9956 2.91357H16.9956V5.91357C16.9956 7.57043 15.6525 8.91357 13.9956 8.91357C12.3388 8.91357 10.9956 7.57043 10.9956 5.91357Z" fill="#FFA202" />
                 <rect x="10.9956" y="11.0864" width="6" height="6" rx="2" fill="#F36C01" />
               </svg>
-              <label htmlFor="my-drawer">Select Food Item</label>
+             
+                
+              <p>Select Food Item</p>
             </div>
+             
             <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
               <path d="M2.1687 4.6314L6.97067 9.43337L11.8312 4.56662" stroke="#212121" strokeWidth="2" strokeLinecap="round" />
             </svg>
           </div>
-
+          </label>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
           <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
-            <SmallSidebar/>
+            <SmallSidebar />
           </ul>
         </div>
       </div>
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -4,13 +4,13 @@ import sunLogo from "../assets/Frame 767.png";
 import control from "../assets/Frame 769.png";
 import bar from "../assets/menu-01.png";
 import glass from "../assets/searchGlass.png";
-import { useState } from "react";
-import { HiMenuAlt2 } from "react-icons/hi";
-import { IoMdCloseCircleOutline } from "react-icons/io";
+// import { useState } from "react";
+// import { HiMenuAlt2 } from "react-icons/hi";
+// import { IoMdCloseCircleOutline } from "react-icons/io";
 
 const NavBar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const toggleMenu = () => setMenuOpen(!menuOpen);
+  // const [menuOpen, setMenuOpen] = useState(false);
+  // const toggleMenu = () => setMenuOpen(!menuOpen);
 
   return (
     <div>
@@ -58,34 +58,34 @@ const NavBar = () => {
 
         {/* sm device start */}
 
-        <div className="lg:hidden block">
-          <div className="cursor-pointer grid grid-cols-12 items-center py-1">
-            <div
-              onClick={toggleMenu}
+        <div className="lg:hidden block mt-5">
+          <div className="cursor-pointer grid grid-cols-12 items-center justify-center h-8">
+            {/* <div
+              onClick={'toggleMenu'}
               className="col-span-2 flex items-starts justify-center"
             >
               <HiMenuAlt2 className="w-9 h-9 text-black ms-1" />
-            </div>
+            </div> */}
             <Link
               to={"/"}
-              className="col-span-8 flex items-center justify-center"
+              className="col-span-12 flex items-center justify-center"
             >
               <img className="w-[70px]" src={logo} alt="" />
             </Link>
-            <div className="col-span-2 mr-1">
+            {/* <div className="col-span-2 mr-1">
               <p className="text-[#0076CE] font-bold rounded-lg border-2 border-blue-500 py-2 w-full text-center text-xs">
                 Login
               </p>
-            </div>
+            </div> */}
             <div
-              className={
-                menuOpen
-                  ? "fixed top-0 left-0 w-[75%] sm:hidden h-screen p-10 ease-in-out duration-500 bg-slate-200"
-                  : "fixed left-[-100%] top-0 p-10 sm:hidden h-screen ease-in-out duration-500"
-              }
+              // className={
+              //   menuOpen
+              //     ? "fixed top-0 left-0 w-[75%] sm:hidden h-screen p-10 ease-in-out duration-500 bg-slate-200"
+              //     : "fixed left-[-100%] top-0 p-10 sm:hidden h-screen ease-in-out duration-500"
+              // }
             >
               {/* cross sine */}
-              <div className="flex w-full items-center justify-between">
+              {/* <div className="flex w-full items-center justify-between">
                 <div>
                   <Link className="/">
                     <img className="w-[100px]" src={logo} alt="" />
@@ -94,7 +94,7 @@ const NavBar = () => {
                 <div onClick={toggleMenu} className="cursor-pointer">
                   <IoMdCloseCircleOutline className="h-6 w-6 text-white" />
                 </div>
-              </div>
+              </div> */}
 
               {/* mobile device menu */}
               <div className="flex-col py-3 font-medium text-white mt-4">
